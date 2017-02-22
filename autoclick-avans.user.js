17ff5f3bd624566acfd6554b8511fafe57d4d227
Daniël Schotman <dschotman@outlook.com>
2017-02-22 13:41:57 +0100

    Update autoclick-avans.user.js

--- autoclick-avans.user.js
+++ autoclick-avans.user.js
@@ -3,6 +3,8 @@
 // @namespace  nl.dschotman.userscript.autoclick
 // @version    1.0
 // @description  SOMETHINGUSEFUL
+// @updateURL      https://github.com/Dediqated/userscripts/raw/master/autoclick-avans.meta.js
+// @downloadURL    https://github.com/Dediqated/userscripts/raw/master/autoclick-avans.user.js
 // @match      https://engine.surfconext.nl/authentication/idp/single-sign-on?*
 // @include    https://engine.surfconext.nl/authentication/idp/single-sign-on?*
 // @require    https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
