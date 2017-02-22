91f61417aaee04f5f46a76e85e6a4a5a89b85d6a
Daniël Schotman <dschotman@outlook.com>
2017-02-22 13:40:12 +0100

    Create autoclick-avans.meta.js

---
+++ autoclick-avans.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name       auto click Avans
+// @namespace  nl.dschotman.userscript.autoclick
+// @version    1.0
+// @description  SOMETHINGUSEFUL
+// @match      https://engine.surfconext.nl/authentication/idp/single-sign-on?*
+// @include    https://engine.surfconext.nl/authentication/idp/single-sign-on?*
+// @require    https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @copyright  2015+, Daniël Schotman
+// ==/UserScript==
